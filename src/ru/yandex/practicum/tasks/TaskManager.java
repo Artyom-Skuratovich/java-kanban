7f9a499b0efcf9c6bee525262ba075a08fd35f6f
Artyom Skuratovich <artyomskuratovich@gmail.com>
2025-03-19 01:05:39 +0300

    Разработал классы задач. Начал работу над классом TaskManager

---
+++ src/ru/yandex/practicum/tasks/TaskManager.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum;
+
+import ru.yandex.practicum.tasks.Task;
+
+import java.util.HashMap;
+
+public class TaskManager {
+    private final HashMap<Integer, Task> ordinaryTasks;
+}
