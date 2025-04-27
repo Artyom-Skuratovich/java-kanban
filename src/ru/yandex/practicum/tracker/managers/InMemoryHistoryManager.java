f107f15a443cf85a1169d44f5a634fdf021170b4
Artyom Skuratovich <artyomskuratovich@gmail.com>
2025-04-27 20:03:53 +0300

    dev: добавить удаление неактуальных id из эпика

--- src/ru/yandex/practicum/tracker/managers/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/tracker/managers/InMemoryHistoryManager.java
@@ -4,30 +4,11 @@ import ru.yandex.practicum.tracker.models.Epic;
 import ru.yandex.practicum.tracker.models.Subtask;
 import ru.yandex.practicum.tracker.models.Task;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private static class Node {
-        final Task data;
-        Node next;
-        Node prev;
-
-        Node(Task data, Node next, Node prev) {
-            this.data = data;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
-
-    private Node head;
-    private Node tail;
-    private final Map<Long, Node> nodeMap;
-
     public InMemoryHistoryManager() {
-        nodeMap = new HashMap<>();
+
     }
 
     @Override
@@ -35,21 +16,11 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (task == null) {
             throw new IllegalArgumentException("Task cannot be null");
         }
-
-        linkLast(task);
-    }
-
-    @Override
-    public void remove(long id) {
-        Node node = nodeMap.remove(id);
-        if (node != null) {
-            removeNode(node);
-        }
     }
 
     @Override
     public List<Task> getHistory() {
-        return getTasks();
+        return List.of();
     }
 
     private Task copyTask(Task task) {
@@ -59,39 +30,5 @@ public class InMemoryHistoryManager implements HistoryManager {
             return new Epic((Epic) task);
         }
         return new Task(task);
-    }
-
-    private List<Task> getTasks() {
-        List<Task> taskList = new ArrayList<>(nodeMap.size());
-        Node current = head;
-
-        while (current != null) {
-            taskList.add(copyTask(current.data));
-            current = current.next;
-        }
-        return taskList;
-    }
-
-    private void linkLast(Task task) {
-        Node node = new Node(copyTask(task), null, tail);
-        if (head == null) {
-            head = node;
-        } else {
-            tail.next = node;
-        }
-        tail = node;
-
-        remove(task.getId());
-        nodeMap.put(task.getId(), node);
-    }
-
-    private void removeNode(Node node) {
-        if (node.prev != null) {
-            node.prev.next = node.next;
-        }
-        if (node.next != null) {
-            node.next.prev = node.prev;
-        }
-        node.next = node.prev = null;
     }
 }
