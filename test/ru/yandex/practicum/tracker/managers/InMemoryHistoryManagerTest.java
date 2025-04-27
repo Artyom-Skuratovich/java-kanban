8da7896e422e0a54fda8e4858734747f35aa4171
Artyom Skuratovich <artyomskuratovich@gmail.com>
2025-04-27 21:01:34 +0300

    dev: добавить тесты для InMemoryHistoryManager

---
+++ test/ru/yandex/practicum/tracker/managers/InMemoryHistoryManagerTest.java
@@ -0,0 +1,59 @@
+package ru.yandex.practicum.tracker.managers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.tracker.models.Epic;
+import ru.yandex.practicum.tracker.models.Subtask;
+import ru.yandex.practicum.tracker.models.Task;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryHistoryManagerTest {
+    private InMemoryHistoryManager historyManager;
+
+    @BeforeEach
+    public void setUp() {
+        historyManager = new InMemoryHistoryManager();
+    }
+
+    @Test
+    public void shouldReturnHistoryWithThreeTasksAfterCreationFourOnesAndRemoveOne() {
+        Task task = new Task("Task", "Task");
+        Subtask subtask = new Subtask("Subtask", "Subtask", 1);
+        subtask.setId(task.getId() + 1);
+        Epic firstEpic = new Epic("Epic", "Epic");
+        firstEpic.setId(subtask.getId() + 1);
+        Epic secondEpic = new Epic(firstEpic);
+        secondEpic.setId(firstEpic.getId() + 1);
+        List<Task> expected = List.of(task, subtask, secondEpic);
+
+        historyManager.add(task);
+        historyManager.add(subtask);
+        historyManager.add(firstEpic);
+        historyManager.add(secondEpic);
+        historyManager.remove(firstEpic.getId());
+
+        List<Task> actual = historyManager.getHistory();
+        assertEquals(expected.size(), actual.size());
+        assertTrue(expected.containsAll(actual));
+
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenNoOneTaskAdded() {
+        assertTrue(historyManager.getHistory().isEmpty());
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTryToCompareReferencesBetweenManualCreatedTaskAndTaskFromHistory() {
+        Epic epic = new Epic("Epic", "Epic");
+        historyManager.add(epic);
+
+        assertNotSame(epic, historyManager.getHistory().getFirst());
+    }
+}
